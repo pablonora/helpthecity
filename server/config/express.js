3b8489f1a6e07747a38930f73654800ba7128b69
André Melo <andre_guilherme_melo@hotmail.com>
2015-09-30 21:43:38 -0300

    Adding WebServer

--- server/config/express.js
+++ server/config/express.js
@@ -1,64 +1,86 @@
 'use strict';
 
 var express = require('express'),
-	glob = require('glob'),
-	favicon = require('serve-favicon'),
-	logger = require('morgan'),
-	cookieParser = require('cookie-parser'),
-	bodyParser = require('body-parser'),
-	compress = require('compression'),
-	methodOverride = require('method-override'),
-	session = require('express-session'),
-	fs = require('fs'),
-	modelInjector = require('../app/middlewares/modelInjector');
+  glob = require('glob'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  compress = require('compression'),
+  methodOverride = require('method-override'),
+  session = require('express-session'),
+  fs = require('fs'),
+  modelInjector = require('../app/middlewares/modelInjector');
 
 module.exports = function (app, config) {
 
-	var env, controllers, daos;
+  var env, controllers, daos;
 
-	env = process.env.NODE_ENV || 'development';
-	app.locals.ENV = env;
-	app.locals.ENV_DEVELOPMENT = env === 'development';
+  env = process.env.NODE_ENV || 'development';
+  app.locals.ENV = env;
+  app.locals.ENV_DEVELOPMENT = env === 'development';
+  // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(cookieParser());
+  app.use(compress());
+  app.use(express.static(config.root + '/public'));
+  app.use(methodOverride());
+  app.use(session({
+    secret: 'supernova',
+    saveUninitialized: true,
+    resave: true
+  }));
+  app.use(modelInjector);
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
-	// app.use(favicon(config.root + '/public/img/favicon.ico'));
-	app.use(logger('dev'));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({
-		extended: true
-	}));
-	app.use(cookieParser());
-	app.use(compress());
-	app.use(express.static(config.root + '/public'));
-	app.use(methodOverride());
-	app.use(session({
-		secret: 'supernova',
-		saveUninitialized: true,
-		resave: true
-	}));
+  // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(cookieParser());
+  app.use(compress());
+  app.use(express.static(config.root + '/public'));
+  app.use(methodOverride());
+  app.use(session({
+    secret: 'supernova',
+    saveUninitialized: true,
+    resave: true
+  }));
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://192.168.4.214:9000');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  //  app.use(function (req, res, next) {
+  //    var err = new Error('Not Found');
+  //    err.status = 404;
+  //    next(err);
+  //  });
+  //  
+  //  if (app.get('env') === 'development') {
+  //    app.use(function (err, req, res, next) {
+  //      res.status(err.status || 500);
+  //      res.send(err);
+  //    });
+  //  }
+  //
+  //  app.use(function (err, req, res, next) {
+  //    res.status(err.status || 500);
+  //      res.end('error');
+  //  });
+  >>> >>> > Stashed changes
 
-	app.use(function (req, res, next) {
-		res.header('Access-Control-Allow-Origin', 'http://192.168.4.214:9000');
-		res.header('Access-Control-Allow-Credentials', true);
-		res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-		next();
-	});
-
-	//  app.use(function (req, res, next) {
-	//    var err = new Error('Not Found');
-	//    err.status = 404;
-	//    next(err);
-	//  });
-	//  
-	//  if (app.get('env') === 'development') {
-	//    app.use(function (err, req, res, next) {
-	//      res.status(err.status || 500);
-	//      res.send(err);
-	//    });
-	//  }
-	//
-	//  app.use(function (err, req, res, next) {
-	//    res.status(err.status || 500);
-	//      res.end('error');
-	//  });
 };
--- server/config/passport.js
+++ server/config/passport.js
@@ -1,77 +1,72 @@
 'use strict';
 
 var passport = require('passport'),
-	LocalStrategy = require('passport-local'),
-	bcrypt = require('bcryptjs');
+  LocalStrategy = require('passport-local'),
+  bcrypt = require('bcryptjs');
 
 module.exports = function (app, config) {
 
-	var auth = require('../app/middlewares/auth');
+  var auth = require('../app/middlewares/auth');
 
-	app.use(passport.initialize());
-	app.use(passport.session());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-	// Passport session setup.
-	//   To support persistent login sessions, Passport needs to be able to
-	//   serialize users into and deserialize users out of the session. Typically,
-	//   this will be as simple as storing the user ID when serializing, and finding
-	//   the user by ID when deserializing.
-	passport.serializeUser(function (user, done) {
-		done(null, user.id);
-	});
+  // Passport session setup.
+  //   To support persistent login sessions, Passport needs to be able to
+  //   serialize users into and deserialize users out of the session.  Typically,
+  //   this will be as simple as storing the user ID when serializing, and finding
+  //   the user by ID when deserializing.
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
 
-	passport.deserializeUser(function (id, done) {
-		return app.daos.User.readById(id).then(function (user) {
-			done(null, user);
-		});
-	});
+  passport.deserializeUser(function (id, done) {
+    return app.daos.User.readById(id).then(function (user) {
+      done(null, user);
+    });
+  });
+  passport.use(new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    function verify(email, password, done) {
+      return app.daos.User.login(email).then(function (user) {
+        if (user == null) return done(null, false, {
+          message: 'User not found'
+        });
+        if (user.length === 0) return done(null, false, {
+          message: 'Unknown user ' + email
+        });
 
-	passport.use(new LocalStrategy({
-			usernameField: 'email',
-			passwordField: 'password'
-		},
-		function verify(email, password, done) {
-			return app.daos.User.login(email).then(function (user) {
-				if (user == null) return done(null, false, {
-					message: 'User not found'
-				});
-				if (user.length === 0) return done(null, false, {
-					message: 'Unknown user ' + email
-				});
-				
-				bcrypt.compare(password, user.password, function (err, res) {
-					if (err) return done(err);
-					if (!res) return done(null, false, {
-						message: 'Invalid password'
-					});
-					done(null, user);
-				});
-			});
-		}
-	));
+        bcrypt.compare(password, user.password, function (err, res) {
+          if (err) return done(err);
+          if (!res) return done(null, false, {
+            message: 'Invalid password'
+          });
+          done(null, user);
+        });
+      });
+    }
+  ));
+  app.post(config.url + '/login', function handleLocalAuthentication(req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+      if (err) return next(err);
+      if (!user) {
+        return res.status(401).send(info.message);
+      }
 
-	app.post(config.url + '/login', function handleLocalAuthentication(req, res, next) {
-		passport.authenticate('local', function (err, user, info) {
-			if (err) return next(err);
-			if (!user) {
-				return res.status(401).send(info.message);
-			}
+      // Manually establish the session...
+      req.login(user, function (err) {
+        if (err) return next(err);
+        return res.send(user);
+      });
+    })(req, res, next);
+  });
 
-			// Manually establish the session...
-			req.login(user, function (err) {
-				if (err) return next(err);
-								
-				user.password = '';
-				
-				return res.send(user);
-			});
-		})(req, res, next);
-	});
-
-	app.delete(config.url + '/logout', auth, function (req, res) {
-		var email = req.user.email;
-		console.log("LOGGIN OUT " + email)
-		req.logout();
-		req.session.notice = "You have successfully been logged out " + email + "!";
-	});
+  app.delete(config.url + '/logout', auth, function (req, res) {
+    var email = req.user.email;
+    console.log('LOGGIN OUT ' + email)
+    req.logout();
+    req.session.notice = 'You have successfully been logged out ' + email + '!';
+  });
 };
